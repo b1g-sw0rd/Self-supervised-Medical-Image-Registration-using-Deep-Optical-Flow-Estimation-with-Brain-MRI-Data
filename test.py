bb9aea94b8162607fea010c83d55b49ae73b573f
Zijian Wang <135504141+b1g-sw0rd@users.noreply.github.com>
2024-07-24 21:58:27 +0200

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-import torch
-device = torch.device('cuda')
-pt = torch.load(r"C:\Users\13660\PycharmProjects\OFE-Reg\PWC\pwc_net.pth.tar", map_location=device)
-pt['conv1a.0.weight'] = pt['conv1a.0.weight'].sum(1, keepdim=True)
